98f49b97a97c5324aa315a1b14e0b8a5db0bdbb2
manish <manishk@winjit.com>
2021-06-24 19:42:11 +0530

    Initial Commit

---
+++ assignment_25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment_25
+//
+//  Created by manish on 24/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
